2369fbb7a012574676aaf52c10a94b24cbb97a7a
Zisouli <ezisouli@deloitte.gr>
2020-08-24 10:04:21 +0300

    Your comment

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+    
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://g2w4mdih46.execute-api.eu-west-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: " us-east-1_DtZHuFC7G",
+      APP_CLIENT_ID: "5vlanfeakudvg9stljm6j1mmp8",
+      IDENTITY_POOL_ID: "eu-west-1:af56d79c-4c9c-4d97-bf70-34c0ff00aa65 "
+    }
+  };
